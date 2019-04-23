f77566379ff4f6c782b16f0707e273af60a5c45d
owencyr <cyroc@dukes.jmu.edu>
2019-04-23 19:18:47 -0400

    formatting leaderboard

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import SmallProfile from '../Profile/SmallProfile';
+import React from "react";
+import SmallProfile from "../Profile/SmallProfile";
 
-export default function LeaderBoard(props){
-    return(
-        <div>
-        <p>We are the leaders, getting our job done!</p>
-        {/* <SmallProfile /> */}
-        </div>
-    )
+export default function LeaderBoard(props) {
+	return (
+		<div>
+			<p>We are the leaders, getting our job done!</p>
+			{/* <SmallProfile /> */}
+		</div>
+	);
 }
