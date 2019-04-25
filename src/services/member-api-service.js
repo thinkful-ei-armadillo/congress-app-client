81673b1b6c8bbb5e5e94a5eb2897010fa378b0bc
owencyr <cyroc@dukes.jmu.edu>
2019-04-25 13:56:09 -0400

    leaderboard prototype for demo

--- src/services/member-api-service.js
+++ src/services/member-api-service.js
@@ -1,55 +1,47 @@
-import config from '../config';
+import config from "../config";
 
 const MemberApiService = {
-  
-  getMemberbyID(id) {
-    return fetch(`http://localhost:8000/api/members/${id}`).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-    );
-  },
+	getMemberbyID(id) {
+		return fetch(`${config.API_ENDPOINT}/members/${id}`).then(res =>
+			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+		);
+	},
 
-  getMembersByState(params) {
-    let url = `${config.API_ENDPOINT}/members`;
-    console.log(params);
-    
-    if (params) {
-      url += `?query=${encodeURIComponent(params)}`;
-    }
-    console.log(url)
-    return fetch(url, {
-      method: 'GET'
-    }).then(res => {
-      if (!res.ok) {
-        res.json().then(e => Promise.reject(e));
-      }
-      else {
-        res.json()
-      }  
-    });
-  },
+	getMembersByState(params) {
+		let url = `${config.API_ENDPOINT}/members`;
+		console.log(params);
 
-  // getMember(id) {
-  // 	let getEndpoint =
-  // 		"https://api.propublica.org/congress/v1/{congress}/{chamber}/members.json";
-  // 	return fetch(getEndpoint + id, {
-  // 		headers: {}
-  // 	}).then(res =>
-  // 		!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-  // );
-  // },
+		if (params) {
+			url += `?query=${encodeURIComponent(params)}`;
+		}
+		console.log(url);
+		return fetch(url, {
+			method: "GET"
+		}).then(res => {
+			if (!res.ok) {
+				res.json().then(e => Promise.reject(e));
+			} else {
+				res.json();
+			}
+		});
+	},
 
-  getMembers(params) {
-    let url = `${config.API_ENDPOINT}/members/search`;
-    if (params.query) {
-      url += `?query=${encodeURIComponent(params.query)}`;
-    }
-    return fetch(url, {
-      method: 'GET'
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-    );
-  },
-
+	getMembers(params) {
+		let url = `${config.API_ENDPOINT}/members/search`;
+		if (params.query) {
+			url += `?query=${encodeURIComponent(params.query)}`;
+		}
+		return fetch(url, {
+			method: "GET"
+		}).then(res =>
+			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+		);
+	},
+	getTop3s() {
+		return fetch(`${config.API_ENDPOINT}/top3s`).then(res =>
+			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+		);
+	}
 };
 
 export default MemberApiService;
