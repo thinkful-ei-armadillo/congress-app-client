48105fcce6b7253632aad538a013e88d129534f6
Payman Kossari <Paymankossari@gmail.com>
2019-04-26 11:39:40 -0400

    fixed bills a bit

--- src/components/CongressStatus/CongressStatus.js
+++ src/components/CongressStatus/CongressStatus.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import CongressApiService from '../../services/congress-api-service';
 import MediumProfile from '../Profile/MediumProfile';
 import MemberApiService from '../../services/member-api-service';
+import './CongressStatus.css';
 
 export default function CongressStatus(props) {
   const [bills, setBills] = useState(null);
@@ -16,18 +17,22 @@ export default function CongressStatus(props) {
     return bills.map(bill => {
       return (
         <li>
-          Bill information w/ sponsor id, short_title, and title:
-          <MediumProfile member={bill.member} />
           <p>{bill.title}</p>
           <p>{bill.latest_major_action}</p>
+          <p>
+            <b>Sponsor</b>
+          </p>
+          <MediumProfile member={bill.member} />
         </li>
       );
     });
   };
   return (
-    <div>
-      <p>What's happening on the floor?</p>
-      <div>{bills && <ul>{renderBills()}</ul>}</div>
+    <div className="congress-status-component">
+      <h3>
+        <b>What's happening on the floor?</b>
+      </h3>
+      <div>{bills && <ul className="bill-list">{renderBills()}</ul>}</div>
     </div>
   );
 }
