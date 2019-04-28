b4f884e8e5f146620c3445281b09e60610109ed2
owencyr <cyroc@dukes.jmu.edu>
2019-04-28 07:42:04 -0400

    working on it

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,23 +9,30 @@ function Dashboard(props) {
 	return (
 		<div className="dashboard">
 			{/* member search will render results component on top of dashboard? */}
-			<MemberSearch />
-			<br />
-			<br />
-			{/* State search component modifies its contents based on dropdown selection */}
-			{/* and will show MediumProfiles on the state's representatives */}
-			{/* will need to show many representatives */}
-			{/* or add district filter for representative display (don't display any members until user has filled both state and district dropdowns?) */}
-			<StateSearch />
-			<br />
-			<br />
+			<div className="col1">
+				<MemberSearch />
+				<br />
+				<br />
+				{/* State search component modifies its contents based on dropdown selection */}
+				{/* and will show MediumProfiles on the state's representatives */}
+				{/* will need to show many representatives */}
+				{/* or add district filter for representative display (don't display any members until user has filled both state and district dropdowns?) */}
+				<StateSearch />
+				<br />
+				<br />
+			</div>
+
 			{/* leaderboard shows sets of 3, at first 1 list */}
 			{/* 3 SmallProfiles showing stat related to leaderboard */}
 			{/* show more than one leaderboard in the future */}
-			<LeaderBoard />
-			<br />
-			<br />
-			<CongressStatus />
+			<div className="col2">
+				<LeaderBoard />
+				<br />
+				<br />
+				<CongressStatus />
+				<br />
+				<br />
+			</div>
 		</div>
 	);
 }
