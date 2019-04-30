956c6adc6c77cf5524494f62df6f0666fe6663f4
Ali Lahrime <alahrime@email.wm.edu>
2019-04-30 11:23:40 -0400

    graph data service for missed votes

---
+++ src/services/graph-data-service.js
@@ -0,0 +1,29 @@
+import config from "../config";
+import MemberApiService from "./member-api-service";
+import CongressApiService from "./congress-api-service";
+
+const graphData = {
+  getNumbers() {
+    let repubArr = [];
+    let demArr = [];
+    let indArr = [];
+    let fullArr = MemberApiService.getMembers();
+    for (let i = 0; i < fullArr.length; i++) {
+      if (fullArr[i].party === "R") {
+        repubArr.push(fullArr[i].missed_votes_pct);
+      }
+      if (fullArr[i].party === "D") {
+        demArr.push(fullArr[i].missed_votes_pct);
+      }
+      if (fullArr[i].party === "ID") {
+        indArr.push(fullArr[i].missed_votes_pct);
+      }
+    }
+    let repubAvg = repubArr.reduce((a, b) => a + b) / repubArr.length;
+    let demAvg = demArr.reduce((a, b) => a + b) / demArr.length;
+    let indAvg = indArr.reduce((a, b) => a + b) / indArr.length;
+    return { repub: repubAvg, dem: demAvg, ind: indAvg };
+  }
+};
+// CongressApiService.getBills(),
+module.exports = { graphData };
