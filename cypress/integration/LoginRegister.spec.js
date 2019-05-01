1f76f890e0aff9b32172581d507afba66500ae03
owencyr <cyroc@dukes.jmu.edu>
2019-05-01 12:41:32 -0400

    login testing wip

--- cypress/integration/LoginRegister.spec.js
+++ cypress/integration/LoginRegister.spec.js
@@ -2,8 +2,11 @@ describe("User Login/Registry", function() {
 	it("Does not do much", function() {
 		expect(true).to.equal(true);
 	});
-	it("visits the login page", function() {
+	it("visits the login page and logs in using the demo account", function() {
 		cy.visit("/login");
+		cy.get("[data-cy=user_name]").type("demo123");
+		cy.get("[data-cy=password]").type("Demo123!");
+		cy.get("[data-cy=login_form]").submit();
 	});
 
 	it("visits the register page", function() {
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,74 +1,73 @@
-import React, { Component } from 'react'
-import { Button, Input } from '../Utils/Utils'
-import AuthApiService from '../../services/auth-api-service'
-import './LoginForm.css'
+import React, { Component } from "react";
+import { Button, Input } from "../Utils/Utils";
+import AuthApiService from "../../services/auth-api-service";
+import "./LoginForm.css";
 
 export default class LoginForm extends Component {
-  static defaultProps = {
-    onLoginSuccess: () => {}
-  }
+	static defaultProps = {
+		onLoginSuccess: () => {}
+	};
 
-  state = { error: null }
+	state = { error: null };
 
-  handleSubmitJwtAuth = ev => {
-    ev.preventDefault()
+	handleSubmitJwtAuth = ev => {
+		ev.preventDefault();
 
-    const { user_name, password } = ev.target
-    
-    this.setState({ error: null })
+		const { user_name, password } = ev.target;
 
-    console.log('login form submitted')
-        
-    
-    AuthApiService.postLogin({
-      user_name: user_name.value,
-      password: password.value,
-    })
-      .then(res => {
-        user_name.value = ''
-        password.value = ''
-        this.props.onLoginSuccess()
-      })
-      .catch(res => {
-        this.setState({ error: res.error })
-      })
-  }
+		this.setState({ error: null });
 
-  render() {
-    const { error } = this.state
-    return (
-      <form
-        className='LoginForm'
-        onSubmit={this.handleSubmitJwtAuth}
-      >
-        <div role='alert'>
-          {error && <p className='red'>{error}</p>}
-        </div>
-        <div className='user_name'>
-          <label htmlFor='LoginForm__user_name'>
-            Username
-          </label><br/>
-          <Input
-            required
-            name='user_name'
-            id='LoginForm__user_name'>
-          </Input>
-        </div><br/>
-        <div className='password'>
-          <label htmlFor='LoginForm__password'>
-            Password
-          </label><br/>
-          <Input
-            required
-            name='password'
-            type='password'
-            id='LoginForm__password'>
-          </Input>
-        </div><br/>
-        <Button type='submit' id='login-button'>
-          Login
-        </Button>
-      </form>
-    )
-  }
+		console.log("login form submitted");
+
+		AuthApiService.postLogin({
+			user_name: user_name.value,
+			password: password.value
+		})
+			.then(res => {
+				user_name.value = "";
+				password.value = "";
+				this.props.onLoginSuccess();
+			})
+			.catch(res => {
+				this.setState({ error: res.error });
+			});
+	};
+
+	render() {
+		const { error } = this.state;
+		return (
+			<form
+				className="LoginForm"
+				onSubmit={this.handleSubmitJwtAuth}
+				data-cy="login_form">
+				<div role="alert">{error && <p className="red">{error}</p>}</div>
+				<div className="user_name">
+					<label htmlFor="LoginForm__user_name">Username</label>
+					<br />
+					<Input
+						required
+						name="user_name"
+						id="LoginForm__user_name"
+						data-cy="user_name"
+					/>
+				</div>
+				<br />
+				<div className="password">
+					<label htmlFor="LoginForm__password">Password</label>
+					<br />
+					<Input
+						required
+						name="password"
+						type="password"
+						id="LoginForm__password"
+						data-cy="password"
+					/>
+				</div>
+				<br />
+				<Button type="submit" id="login-button">
+					Login
+				</Button>
+			</form>
+		);
+	}
 }
