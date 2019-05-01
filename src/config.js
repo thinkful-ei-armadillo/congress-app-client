d172dfa28b558da98c51a59b1548715334007f58
owencyr <cyroc@dukes.jmu.edu>
2019-05-01 11:35:39 -0400

    api endpoint changed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
 	// API_ENDPOINT: 'http://localhost:8000/api',
-	API_ENDPOINT: "https://congress-io.herokuapp.com/",
+	API_ENDPOINT: "https://congress-io.herokuapp.com/api",
 	TOKEN_KEY: "congress-client-auth-token"
 };
