75ccf293cbe843510068fe474a96c7fafc221b41
Ali Lahrime <alahrime@email.wm.edu>
2019-05-02 00:42:34 -0400

    got pie chart working, just need to pass props

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import MemberSearch from '../Member/MemberSearch';
-import StateSearch from '../Member/StateSearch';
-import LeaderBoard from '../LeaderBoard/LeaderBoard';
-import CongressStatus from '../CongressStatus/CongressStatus';
+import React from "react";
+import MemberSearch from "../Member/MemberSearch";
+import StateSearch from "../Member/StateSearch";
+import LeaderBoard from "../LeaderBoard/LeaderBoard";
+import CongressStatus from "../CongressStatus/CongressStatus";
 import Committees from "../Committees/Committees";
-import './Dashboard.css';
-import MissedVotesGraph from '../Data/MissedVotesGraph';
+import "./Dashboard.css";
+import MissedVotesGraph from "../Data/MissedVotesGraph";
+import ProfileVotes from "../Data/ProfileVotes";
 
 function Dashboard(props) {
-
   return (
-    <div className='dashboard'>
-      <div className='col1'>
+    <div className="dashboard">
+      <div className="col1">
         <MemberSearch />
         <br />
         <br />
@@ -21,14 +21,17 @@ function Dashboard(props) {
         <MissedVotesGraph />
         <br />
         <br />
+        <ProfileVotes props={0.5} />
+        <br />
+        <br />
         <CongressStatus />
         <br />
         <br />
         <Committees />
-				<br />
-				<br />
+        <br />
+        <br />
       </div>
-      <div className='col2'>
+      <div className="col2">
         <LeaderBoard />
         <br />
         <br />
--- src/components/Data/ProfileVotes.js
+++ src/components/Data/ProfileVotes.js
@@ -3,35 +3,78 @@ import * as d3 from "d3";
 
 export default class ProfileVotes extends Component {
   componentDidMount() {
-    const width = 480;
-    const height = 225;
-    const radius = Math.min(width, height) / 2;
+    const values = Object.values(this.props);
+    console.log(values);
+    values.push(1 - values[0]);
+    console.log(values);
+    const data = values.map(num => num * 100);
 
-    const svgGraph = d3
+    const width = 300;
+    const height = 300;
+    const radius = Math.min(width, height) / 2;
+    const color = d3.scaleOrdinal().range(["red", "blue"]);
+
+    const pie = d3.pie().value(function(d) {
+      return d;
+    })(data);
+    const arc = d3
+      .arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+    const labelArc = d3
+      .arc()
+      .outerRadius(radius - 40)
+      .innerRadius(radius - 40);
+    const svg = d3
       .select("#profile-votes-graph")
       .append("svg")
       .attr("width", width)
-      .attr("height", height);
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    const g = svg
+      .selectAll("arc")
+      .data(pie)
+      .enter()
+      .append("g")
+      .attr("class", "arc");
 
-    const pie = d3.layout
-      .pie()
-      .sort(null)
-      .value(function(d) {
-        return d;
+    g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) {
+        return color(d.data);
       });
-
-    const arc = d3.svg
-      .arc()
-      .outerRadius(radius * 0.6)
-      .innerRadius(radius * 0.3);
-    const outerArc = d3.svg
-      .arc()
-      .outerRadius(radius * 0.5)
-      .innerRadius(radius * 0.5);
   }
 
   render() {
-    let dataset = this.props;
+    // let dataset = this.props;
     return <div id="profile-votes-graph" />;
   }
 }
+
+// const width = 480;
+// const height = 225;
+// const radius = Math.min(width, height) / 2;
+
+// const svgGraph = d3
+//   .select("#profile-votes-graph")
+//   .append("svg")
+//   .attr("width", width)
+//   .attr("height", height);
+
+// const pie = d3.layout
+//   .pie()
+//   .sort(null)
+//   .value(function(d) {
+//     return d;
+//   });
+
+// const arc = d3.svg
+//   .arc()
+//   .outerRadius(radius * 0.6)
+//   .innerRadius(radius * 0.3);
+// const outerArc = d3.svg
+//   .arc()
+//   .outerRadius(radius * 0.5)
+//   .innerRadius(radius * 0.5);
