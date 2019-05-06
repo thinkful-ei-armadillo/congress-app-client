bb8e72b6e6ff96603f419aa26a45cfac98849cdf
owencyr <cyroc@dukes.jmu.edu>
2019-05-06 15:50:29 -0400

    test file structure, cy.login

---
+++ cypress/integration/member/Profile.spec.js
@@ -0,0 +1,9 @@
+// check all props display
+
+// check normal profile
+// check medium profile
+
+// check small profile
+
+// check compare/follow buttons appear
+// (is this part of compare/follow tests instead?)
---
+++ cypress/integration/shared/Header.spec.js
@@ -0,0 +1,1 @@
+// test all nav buttons, title content
