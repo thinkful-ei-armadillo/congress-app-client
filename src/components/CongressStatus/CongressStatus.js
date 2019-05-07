a6e1bf6437bbc80b41486fa47c4b5582623037b0
Payman Kossari <Paymankossari@gmail.com>
2019-05-07 12:11:08 -0400

    readme updated

--- src/components/CongressStatus/CongressStatus.js
+++ src/components/CongressStatus/CongressStatus.js
@@ -58,7 +58,6 @@ export default function CongressStatus(props) {
         <select
           className="bill-filter"
           onChange={e => {
-            debugger;
             setBillFilter(e.target.value);
           }}
           value={billFilter}
