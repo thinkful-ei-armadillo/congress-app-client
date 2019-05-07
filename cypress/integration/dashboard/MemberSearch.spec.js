192555ce2e562aaa2da2ab4223f46df05f0fe8bb
Irem Sen <secilreel@yahoo.com>
2019-05-07 12:10:17 -0400

    updated member search test

--- cypress/integration/dashboard/MemberSearch.spec.js
+++ cypress/integration/dashboard/MemberSearch.spec.js
@@ -13,21 +13,6 @@ describe("MemberSearch Happy", function() {
 		cy.get("[data-cy=search]").type("alex{enter}");
 		cy.get("[data-cy=search-result]").contains("Alex")
 		cy.get("[data-cy=search-result] > :nth-child(1) > a").click();
-		cy.get(".main-profile").within($section => {
-			cy.get(".profile-picture");
-			cy.get(".name");
-			cy.get(".dob");
-			cy.get(".member-social-media");
-			cy.get(".seniority");
-			cy.get(".next_election");
-			cy.get(".total_votes");
-			cy.get(".missed_votes");
-			cy.get(".missed_votes_pct");
-			cy.get("#profile-missed-votes-graph");
-			cy.get(".votes_with_party_pct");
-			cy.get("#profile-party-votes-graph");
-			cy.get("[href='/compare']");
-		});
 	});
 });
 describe("MemberSearch Unhappy", function() {
