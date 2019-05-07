d63e69d63a8cb96c9d05dfa0929c18743c5aaf02
owencyr <cyroc@dukes.jmu.edu>
2019-05-07 11:11:16 -0400

    profile tests

--- cypress/integration/member/Profile.spec.js
+++ cypress/integration/member/Profile.spec.js
@@ -1,16 +1,49 @@
 // check all props display
 describe("Profile", function() {
-	it("views Profile of given user and checks all props are there", function() {
-		cy.visit("/");
+	it("views Profile of given member not logged in", function() {
+		cy.visit("/profile/R000600", {
+			onBeforeLoad: win => {
+				win.sessionStorage.clear();
+			}
+		});
+		cy.get(".main-profile").within($section => {
+			cy.get(".profile-picture");
+			cy.get(".name");
+			cy.get(".dob");
+			cy.get(".member-social-media");
+			cy.get(".seniority");
+			cy.get(".next_election");
+			cy.get(".total_votes");
+			cy.get(".missed_votes");
+			cy.get(".missed_votes_pct");
+			cy.get("#profile-missed-votes-graph");
+			cy.get(".votes_with_party_pct");
+			cy.get("#profile-party-votes-graph");
+			cy.get("[href='/compare']");
+		});
 	});
-	it("views Profile of given user", function() {
+	it("views Profile of given member logged in", function() {
+		cy.login();
 		cy.visit("/profile/R000600");
+		cy.get(".main-profile").within($section => {
+			cy.get(".profile-picture");
+			cy.get(".name");
+			cy.get(".dob");
+			cy.get(".member-social-media");
+			cy.get(".seniority");
+			cy.get(".next_election");
+			cy.get(".total_votes");
+			cy.get(".missed_votes");
+			cy.get(".missed_votes_pct");
+			cy.get("#profile-missed-votes-graph");
+			cy.get(".votes_with_party_pct");
+			cy.get("#profile-party-votes-graph");
+			cy.get("[href='/compare']");
+			cy.get(".add-member-following");
+		});
 	});
 });
 // check normal profile
 // check medium profile
 
 // check small profile
-
-// check compare/follow buttons appear
-// (is this part of compare/follow tests instead?)
