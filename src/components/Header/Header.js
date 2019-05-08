fe99415dbd29a814da3f9397e9b1a7e2ee791165
owencyr <cyroc@dukes.jmu.edu>
2019-05-08 17:46:20 -0400

    janky logo positioning

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,71 +1,74 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '../Utils/Utils';
-import TokenService from '../../services/token-service';
-import UserContext from '../../contexts/UserContext';
-import './Header.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../Utils/Utils";
+import TokenService from "../../services/token-service";
+import UserContext from "../../contexts/UserContext";
+import "./Header.css";
 
 export default class Header extends Component {
-  static contextType = UserContext;
+	static contextType = UserContext;
 
-  handleLogoutClick = () => {
-    this.context.processLogout();
-  };
+	handleLogoutClick = () => {
+		this.context.processLogout();
+	};
 
-  renderLogoutLink() {
-    // console.log(this.context.user.username);
-    return (
-      <div className="logged-in-header">
-        {/* <p>Welcome, {this.context.user.username}</p> */}
-        {'  '}
-        <Link to="/compare">
-          <Button id="compare-button">Compare Members</Button>
-        </Link>
-        <Link onClick={this.handleLogoutClick} to="/">
-          <Button id="logout-button">Logout</Button>
-        </Link>
-      </div>
-    );
-  }
+	renderLogoutLink() {
+		// console.log(this.context.user.username);
+		return (
+			<div className="logged-in-header">
+				{/* <p>Welcome, {this.context.user.username}</p> */}
+				{"  "}
+				<Link to="/compare">
+					<Button id="compare-button">Compare Members</Button>
+				</Link>
+				<Link onClick={this.handleLogoutClick} to="/">
+					<Button id="logout-button">Logout</Button>
+				</Link>
+			</div>
+		);
+	}
 
-  renderLoginLink() {
-    return (
-      <div className="logged-out-header">
-        <Link className="compare-button-link" to="/compare">
-          <Button id="compare-button">Compare Members</Button>
-        </Link>{' '}
-        <Link to="/login" className="login-button">
-          <Button id="login-button">Login</Button>
-        </Link>{' '}
-        <Link to="/register" className="register-button">
-          <Button id="register-button">Register</Button>
-        </Link>
-      </div>
-    );
-  }
+	renderLoginLink() {
+		return (
+			<div className="logged-out-header">
+				<Link className="compare-button-link" to="/compare">
+					<Button id="compare-button">Compare Members</Button>
+				</Link>{" "}
+				<Link to="/login" className="login-button">
+					<Button id="login-button">Login</Button>
+				</Link>{" "}
+				<Link to="/register" className="register-button">
+					<Button id="register-button">Register</Button>
+				</Link>
+			</div>
+		);
+	}
 
-  render() {
-    return (
-      <header>
-        <h1 className="title">
-          {TokenService.hasAuthToken() ? (
-            <Link to="/dashboard">
-              <span className="little">My</span>Congress
-              <span className="little">.io</span>
-            </Link>
-          ) : (
-            <Link to="/">
-              <span className="little">My</span>Congress
-              <span className="little">.io</span>
-            </Link>
-          )}
-        </h1>
-        <nav>
-          {TokenService.hasAuthToken()
-            ? this.renderLogoutLink()
-            : this.renderLoginLink()}
-        </nav>
-      </header>
-    );
-  }
+	render() {
+		return (
+			<header>
+				<div className="logo-container">
+					<img src="https://i.ibb.co/LY5d7h2/logo.png" alt="logo" border="0" />
+				</div>
+				<h1 className="title">
+					{TokenService.hasAuthToken() ? (
+						<Link to="/dashboard">
+							<span className="little">My</span>Congress
+							<span className="little">.io</span>
+						</Link>
+					) : (
+						<Link to="/">
+							<span className="little">My</span>Congress
+							<span className="little">.io</span>
+						</Link>
+					)}
+				</h1>
+				<nav>
+					{TokenService.hasAuthToken()
+						? this.renderLogoutLink()
+						: this.renderLoginLink()}
+				</nav>
+			</header>
+		);
+	}
 }
