daa89fbd1945abdca0194b55ce94f013f7fe7ee9
Daniel Ors <do7811@gmail.com>
2019-05-08 17:21:02 -0400

    completed mobile version of leaderboard

---
+++ src/components/LeaderBoard/LeaderBoardMobile.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import SmallProfile from "../Profile/SmallProfile";
+import MemberApiService from "../../services/member-api-service";
+import "./LeaderBoard.css";
+
+export default function LeaderBoardMobile(props) {
+	let [top3, setTop3] = useState(null);
+	useEffect(() => {
+		// query members by one prop, show them sorted DESC
+		MemberApiService.getTop3s().then(res => setTop3(res));
+	}, []);
+	return top3 ? (
+    <>
+    <p className="LeaderBoard-title-mobile">
+				<b>Leaders in Missed Votes</b>
+		</p>
+		<div className="LeaderBoardMobile">
+			{top3.map((member, index) => {
+				return (
+					<div key={member.id} className="MemberContainer">
+						<h4 className="rank">#{index + 1}</h4>
+						<SmallProfile member={member} className="leaderboard-small-profile" />
+					</div>
+				);
+			})}
+		</div>
+    </>
+	) : (
+		""
+	);
+}
